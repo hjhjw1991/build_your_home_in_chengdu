f15daf71b669dc20445aabc8d1c73bbeb7fddffb
huangjun.barney <huangjun.barney@bytedance.com>
2021-12-03 17:55:26 +0800

    WIP: 尝试给class添加getter方法, 后续改为给class注入服务配置

--- android/serviceprovider-annotation/src/main/java/com/hjhjw1991/barney/serviceprovider/annotation/ServiceProcessor.kt
+++ android/serviceprovider-annotation/src/main/java/com/hjhjw1991/barney/serviceprovider/annotation/ServiceProcessor.kt
@@ -3,15 +3,29 @@ package com.hjhjw1991.barney.serviceprovider.annotation
 
 import com.hjhjw1991.barney.serviceprovider.annotation.ServiceProcessor.Companion.SERVICE_IMPL
 import com.hjhjw1991.barney.serviceprovider.annotation.ServiceProcessor.Companion.SERVICE_SPI
+import com.squareup.javapoet.ClassName
+import com.squareup.javapoet.JavaFile
+import com.squareup.javapoet.MethodSpec
+import com.squareup.javapoet.TypeSpec
+import com.sun.source.tree.Tree
+import com.sun.source.util.Trees
+import com.sun.tools.javac.code.Flags
 import com.sun.tools.javac.code.Type
+import com.sun.tools.javac.code.Types
+import com.sun.tools.javac.processing.JavacProcessingEnvironment
+import com.sun.tools.javac.tree.JCTree
+import com.sun.tools.javac.tree.TreeMaker
+import com.sun.tools.javac.tree.TreeTranslator
+import com.sun.tools.javac.util.List as JList
+import com.sun.tools.javac.util.ListBuffer
+import com.sun.tools.javac.util.Name
+import com.sun.tools.javac.util.Names
 import java.io.IOException
-import javax.annotation.processing.AbstractProcessor
-import javax.annotation.processing.ProcessingEnvironment
-import javax.annotation.processing.RoundEnvironment
-import javax.annotation.processing.SupportedAnnotationTypes
+import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
-import javax.lang.model.element.ExecutableElement
+import javax.lang.model.element.Modifier
 import javax.lang.model.element.TypeElement
+import javax.lang.model.util.Elements
 
 @SupportedAnnotationTypes(SERVICE_SPI, SERVICE_IMPL)
 class ServiceProcessor: AbstractProcessor() {
@@ -21,6 +35,19 @@ class ServiceProcessor: AbstractProcessor() {
         const val SERVICE_PROXY = "ServiceManager_Proxy"
         val serviceMapConfig = mutableMapOf<String, MutableSet<String>>()
     }
+    // apt 相关类
+    protected val filer: Filer get() = EnvUtil.filer
+    protected val elements: Elements get() = EnvUtil.elements
+    protected val messager: Messager get() = EnvUtil.messager
+
+    // javac 编译器相关类
+    protected val trees: Trees get() = EnvUtil.trees
+    protected val treeMaker: TreeMaker get() = EnvUtil.treeMaker
+    protected val names: Names get() = EnvUtil.names
+
+    private var mFields: MutableList<JCTree.JCVariableDecl> = JList.nil()
+
+    protected var rootTree: JCTree.JCCompilationUnit? = null
 
     override fun process(
         annotations: MutableSet<out TypeElement>?,
@@ -42,20 +69,19 @@ class ServiceProcessor: AbstractProcessor() {
         roundEnv.getElementsAnnotatedWith(ServiceImpl::class.java)
             .filterIsInstance<TypeElement>()
             .forEach { element ->
-//                val treePath = trees.getPath(it)
-//                val cu = treePath.compilationUnit as JCTree.JCCompilationUnit
-//                rootTree = cu
-//                println("process find class = $it, jcTree = ${cu.javaClass.simpleName}")
-                println("process find class = $element")
+                val treePath = trees.getPath(element)
+                val cu = treePath.compilationUnit as JCTree.JCCompilationUnit
+                rootTree = cu
+                println("process find class = $element, jcTree = ${cu.javaClass.simpleName}")
                 element.interfaces.filterIsInstance<Type>()
                     .find { it.tsym.qualifiedName.toString() in serviceMapConfig }
                     ?.let {
                     serviceMapConfig[it.tsym.qualifiedName.toString()]?.add(element.qualifiedName.toString())
                 }
-                translate(element)
+                translate(element, trees.getTree(element) as JCTree)
 
                 try {
-                    generateJavaFile(element)
+                    generateJavaFile(element, trees.getTree(element) as JCTree)
                 } catch (e: IOException) {
                     e.printStackTrace()
                 }
@@ -66,25 +92,36 @@ class ServiceProcessor: AbstractProcessor() {
         return true
     }
 
-    fun translate(curElement: TypeElement) {
+    fun translate(curElement: TypeElement, curTree: JCTree) {
         println("translate")
-        var useDCL = false
-        var executableElement: ExecutableElement? = null
-        curElement.enclosedElements
-            .filter { it is ExecutableElement }
-            .map { it as ExecutableElement }
-            .forEach {
-                if (it.getAnnotation(ServiceInterface::class.java) != null && !it.parameters.isEmpty()) {
-                    useDCL = true
-                    executableElement = it
-                    return@forEach
-                }
-            }
+        curTree.accept(MyTreeTranslator(curElement.simpleName as Name))
     }
 
     @Throws(IOException::class)
-    fun generateJavaFile(curElement: TypeElement) {
+    fun generateJavaFile(curElement: TypeElement, curTree: JCTree) {
         println("generateJavaFile")
+        getJavaFile(curElement).writeTo(filer)
+    }
+
+    fun getJavaFile(curElement: TypeElement): JavaFile {
+        val packageName = rootTree?.packageName?.toString() ?: ""
+
+        val curIGetter = TypeSpec.interfaceBuilder("${curElement.qualifiedName.substring(packageName.length + 1).replace(".", "$$")}\$\$IGetter")
+            .addModifiers(Modifier.PUBLIC)
+            .apply {
+                mFields.forEach {
+                    this.addMethod(
+                        MethodSpec.methodBuilder(it.name.toString())
+                        .addModifiers(Modifier.PUBLIC, Modifier.ABSTRACT)
+                        .returns(ClassName.get(it.sym.asType()))
+                        .build()
+                    )
+                }
+            }
+            .build()
+
+        return JavaFile.builder(packageName, curIGetter)
+            .build()
     }
 
     override fun getSupportedOptions(): MutableSet<String> {
@@ -98,5 +135,72 @@ class ServiceProcessor: AbstractProcessor() {
     override fun init(processingEnv: ProcessingEnvironment) {
         super.init(processingEnv)
         println("HJSPI init module: " + processingEnv.options["MODULE_NAME"])
+        EnvUtil.init(processingEnv)
+    }
+
+    inner class MyTreeTranslator(private val rootClazzName: Name) : TreeTranslator() {
+        override fun visitClassDef(jcClassDecl: JCTree.JCClassDecl) {
+            if (jcClassDecl.name == rootClazzName) {
+                mFields = JList.nil()
+                jcClassDecl.defs
+                    .filter { it.kind == Tree.Kind.VARIABLE }
+                    .map { it as JCTree.JCVariableDecl }
+                    .forEach {
+                        mFields.add(it)
+                        jcClassDecl.defs.add(makeGetterMethodDecl(it))
+                    }
+            }
+            super.visitClassDef(jcClassDecl)
+        }
+
+        /**
+         *   public String getName() {
+         *      return this.name;
+         *   }
+         */
+        private fun makeGetterMethodDecl(jcVariableDecl: JCTree.JCVariableDecl): JCTree.JCMethodDecl {
+            val body = ListBuffer<JCTree.JCStatement>()
+                .append(treeMaker.Return(treeMaker.Select(treeMaker.Ident(names._this), jcVariableDecl.getName())))
+                .toList()
+                .let { treeMaker.Block(0, it) }
+
+            return treeMaker.MethodDef(
+                treeMaker.Modifiers(Flags.PUBLIC.toLong()),
+                names.fromString(jcVariableDecl.getName().toString()),
+                jcVariableDecl.vartype,
+                JList.nil(), JList.nil(), JList.nil(), body, null)
+        }
+    }
+}
+
+internal object EnvUtil {
+    private var env: ProcessingEnvironment? = null
+
+    // apt 相关类
+    lateinit var filer: Filer              //文件相关的辅助类
+    lateinit var elements: Elements    //元素相关的辅助类
+    lateinit var messager: Messager        //日志相关的辅助类
+
+    // javac 编译器相关类
+    lateinit var trees: Trees
+    lateinit var treeMaker: TreeMaker
+    lateinit var names: Names
+    lateinit var types: Types
+
+    fun init(env: ProcessingEnvironment) {
+        if (EnvUtil.env != null) {
+            return
+        }
+        EnvUtil.env = env
+
+        filer = env.filer
+        elements = env.elementUtils
+        messager = env.messager
+
+        trees = Trees.instance(env)
+        val context = (env as JavacProcessingEnvironment).context
+        treeMaker = TreeMaker.instance(context)
+        names = Names.instance(context)
+        types = Types.instance(context)
     }
 }
--- android/serviceprovider/src/main/java/com/hjhjw1991/barney/serviceprovider/ServiceManager.kt
+++ android/serviceprovider/src/main/java/com/hjhjw1991/barney/serviceprovider/ServiceManager.kt
@@ -1,17 +1,49 @@
 package com.hjhjw1991.barney.serviceprovider
 
+import androidx.annotation.Keep
 import java.util.concurrent.ConcurrentHashMap
 
+@Keep
 object ServiceManager {
     var serviceMap: ConcurrentHashMap<Class<*>, Any> = ConcurrentHashMap()
     var servicesMap: ConcurrentHashMap<Class<*>, Set<*>> = ConcurrentHashMap()
 
     fun <T> getService(clazz: Class<out T>): T? {
-        return serviceMap[clazz] as? T
+        var instance: T? = serviceMap[clazz] as? T
+        if (instance == null) {
+            instance = createServiceFor(clazz)
+            if (instance != null) {
+                serviceMap[clazz] = instance as Any
+            }
+        }
+        return instance
+    }
+
+    private fun <T> createServiceFor(name: Class<T>): T? {
+        val configs = ConfigCenter.getConfig(name.name)
+        return configs.firstOrNull()
+            ?.runCatching {
+                Class.forName(this).newInstance() as T
+            }
+            ?.getOrNull()
+    }
+
+    private fun <T> createServicesFor(name: Class<T>): Set<T>? {
+        val configs = ConfigCenter.getConfig(name.name)
+        return configs.map {
+            Class.forName(it).newInstance() as T
+        }.toSet()
     }
 
     fun <T> getServices(clazz: Class<out T>): Set<T>? {
-        return servicesMap[clazz] as? Set<T>
+        var instance: Set<T>? = servicesMap[clazz] as? Set<T>
+        if (instance == null) {
+            instance = createServicesFor(clazz)
+            if (instance != null) {
+                serviceMap[clazz] = instance as Any
+            }
+        }
+        return instance
     }
 
     fun <T> bindService(clazz: Class<in T>, instance: T) {
